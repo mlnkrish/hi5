8c007bbdaed64f540aea402828f3b2b3b530d9c7
Hari Balamani <linkwithhari@gmail.com>
2014-02-22 19:19:35 +0530

    Hari| app.js delegates to route definition for any resources

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var home = require('./routes/home');
+var user = require('./routes/user');
 
-var redis = require("redis");
 var url = require('url');
 
-if(!!process.env.REDISCLOUD_URL) {
-  var redisURL = url.parse(process.env.REDISCLOUD_URL);
-  var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-  client.auth(redisURL.auth.split(":")[1]);
-} else {
-  var client = redis.createClient();
-}
+
+
 
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.get('/', home.index);
+app.post('/register', user.register);
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
---
+++ routes/user.js
@@ -0,0 +1,3 @@
+exports.register = function(req, res) {
+  res.send('registered!');
+};
